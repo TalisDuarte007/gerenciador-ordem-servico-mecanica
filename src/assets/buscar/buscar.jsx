275b6f5bff6fd95ef8818c598a348969669f96a3
TalisDuarte007 <tcd07@hotmail.com>
2024-03-04 23:49:17 -0300

    atualizei algumas estilizações da pagina

--- src/assets/buscar/buscar.jsx
+++ src/assets/buscar/buscar.jsx
@@ -1,51 +1,84 @@
-import React, { useState } from 'react';
-import TabelaOrdens from '../../components/TabelaOrdens';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import TabelaOrdens from "../../components/TabelaOrdens";
 
 export default function SearchComponent(props) {
+  const navigate = useNavigate();
   const dadosDoDB = props.propDB;
-  const [nome, setNome] = useState('');
-  const [veiculo, setVeiculo] = useState('');
-  const [mes, setMes] = useState('');
-  const [ano, setAno] = useState('');
+  const [nome, setNome] = useState("");
+  const [veiculo, setVeiculo] = useState("");
+  const [mes, setMes] = useState("");
+  const [ano, setAno] = useState("");
   const [resultados, setResultados] = useState([]);
 
-  console.log(dadosDoDB)
+  console.log(dadosDoDB);
 
   const handleSearch = () => {
-    const resultadosFiltrados = dadosDoDB.filter(objeto => {
+    const resultadosFiltrados = dadosDoDB.filter((objeto) => {
       // Filtra com base nos critérios de busca (nome, veículo, mês e ano da data)
       return (
-        (nome === '' || objeto.nome.toLowerCase().includes(nome.toLowerCase())) &&
-        (veiculo === '' || objeto.veiculo.toLowerCase().includes(veiculo.toLowerCase())) &&
-        (mes === '' || new Date(objeto.data).getMonth() + 1 === parseInt(mes)) &&
-        (ano === '' || new Date(objeto.data).getFullYear() === parseInt(ano))
+        (nome === "" ||
+          objeto.nome.toLowerCase().includes(nome.toLowerCase())) &&
+        (veiculo === "" ||
+          objeto.veiculo.toLowerCase().includes(veiculo.toLowerCase())) &&
+        (mes === "" ||
+          new Date(objeto.data).getMonth() + 1 === parseInt(mes)) &&
+        (ano === "" || new Date(objeto.data).getFullYear() === parseInt(ano))
       );
     });
     setResultados(resultadosFiltrados);
   };
-  console.log(resultados)
+  console.log(resultados);
 
   return (
-    <div className='content'>
+    <div className="content">
       <h2>Buscar Ordens</h2>
-      <div>
-        <label htmlFor='nome'>Nome:</label>
-        <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
+      <div className="label-box">
+        <label htmlFor="nome">Nome:</label>
+        <input
+          type="text"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          className="input-box"
+        />
       </div>
-      <div>
-        <label htmlFor='veiculo'>Veículo:</label>
-        <input type="text" value={veiculo} onChange={(e) => setVeiculo(e.target.value)} />
+      <div className="label-box">
+        <label htmlFor="veiculo">Veículo:</label>
+        <input
+          type="text"
+          value={veiculo}
+          onChange={(e) => setVeiculo(e.target.value)}
+          className="input-box"
+        />
       </div>
-      <div>
-        <label htmlFor='mes'>Mês:</label>
-        <input type="text" value={mes} onChange={(e) => setMes(e.target.value)} />
+      <div className="label-box">
+        <label htmlFor="mes">Mês:</label>
+        <input
+          type="text"
+          value={mes}
+          onChange={(e) => setMes(e.target.value)}
+          className="input-box"
+        />
       </div>
-      <div>
-        <label htmlFor='ano'>Ano:</label>
-        <input type="text" value={ano} onChange={(e) => setAno(e.target.value)} />
+      <div className="label-box">
+        <label htmlFor="ano">Ano:</label>
+        <input
+          type="text"
+          value={ano}
+          onChange={(e) => setAno(e.target.value)}
+          className="input-box"
+        />
       </div>
-      <button onClick={handleSearch} className='btn btn-pesquisar'>Buscar</button>
-      <TabelaOrdens propDB={resultados}/>
+      <div className="buttons">
+        <button onClick={handleSearch} className="btn btn-pesquisar">
+          Buscar
+        </button>
+        <button onClick={() => navigate("/")} className="btn btn-pesquisar">
+          Cancelar
+        </button>
+      </div>
+
+      <TabelaOrdens propDB={resultados} />
     </div>
   );
 }
